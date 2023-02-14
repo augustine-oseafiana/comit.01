928296b6ac5a85142065bf19817bce8a6cf216ce
Augustine.Oseafiana <augustine.oseafiana@gmail.com>
2023-02-14 00:56:57 -0600

    first version

---
+++ src/main/java/com/comit/course/Practice.java
@@ -0,0 +1,25 @@
+package com.comit.course;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s = "Hi, i already print sentence";
+		
+		      System.out.println(s);
+		
+		int a = 273;
+		
+		   System.out.println(a);
+		
+		float f = 5.3F;
+		     
+		     System.out.println(f);
+		
+		     
+        
+        
+	}
+
+}
