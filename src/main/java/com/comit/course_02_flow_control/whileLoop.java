593e0ffc2379ee597ee1ec33e87b3edd0c4985ed
Augustine.Oseafiana <augustine.oseafiana@gmail.com>
2023-02-21 20:48:02 -0600

    add whileLoop

--- src/main/java/com/comit/course_02_flow_control/whileLoop.java
+++ src/main/java/com/comit/course_02_flow_control/whileLoop.java
@@ -3,27 +3,33 @@ package com.comit.course_02_flow_control;
 public class whileLoop {
 
 	/* Loops can execute a block of code while a condition is true
-	 * 
+	 * Read page 77-81
 	 */
 	public static void main(String[] args) {
 		
 		int i = 0;
 		
-		/*
+		
 		while(i < 10) {
 			
-			System.out.println(i);
 			++i;
+		    
+			if (i==5) {
+				//break; breaks out the loop
+				continue; // continue with the next iteration
+			}
+			
+			System.out.println(i);
+		}
 		
-		}*/
+		i = 0;
 		
-		i=0;
-		
+		/*
 		do {
 			System.out.println(i);
 			++i;	
 		}while (i < 10);
-
+       */
 		System.out.println("The end");
 	}
 
