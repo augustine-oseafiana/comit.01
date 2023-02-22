6938cbaccb48c13fdc9857ca63669a1c02b88daf
Augustine.Oseafiana <augustine.oseafiana@gmail.com>
2023-02-22 13:57:08 -0600

    add forloop

--- src/main/java/com/comit/course_02_flow_control/ForLoop.java
+++ src/main/java/com/comit/course_02_flow_control/ForLoop.java
@@ -1,10 +1,24 @@
 package com.comit.course_02_flow_control;
 
 public class ForLoop {
-
+	
+	/* 
+	 * The for-loop is used when you know exactly how many times you want
+	 * to loop through a block of code.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    
+		int i = 0; 
+	    while(i < 10) {	
+			System.out.println(i);
+			++i;	
+			
+		}
 
+	      /* initialization; termination; increment */
+	    for(int j= 0; j < 10; j++) {
+	    	System.out.println(j);
+	    }
 	}
 
 }
